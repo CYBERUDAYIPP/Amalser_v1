153bb7086a0a4d37f1530e09726714158c76ec44
CYBER UDAYIPP <88606977+CYBERUDAYIPP@users.noreply.github.com>
2021-08-18 22:25:51 +0530

    Update owner.js

--- plugins/owner.js
+++ plugins/owner.js
@@ -1,5 +1,5 @@
-/* Copyright (C) 2021 AFNAN PALLIKKEL
-CODDED AFNAN PALLIKKEL
+/* Copyright (C) 2021 CYBER UDAYIPP
+CODDED CYBER UDAYIPP
 Licensed under the  GPL-3.0 License;
 you may not use this file except in compliance with the License.
 PINKY
@@ -25,7 +25,7 @@ if (Config.WORKTYPE == 'private') {
         }
 
         if (Config.PLK == 'default') {
-            await message.client.sendMessage(message.jid,'Amalser Bot created by *Amal*' , MessageType.text);
+            await message.client.sendMessage(message.jid,'SARASU Bot created by *CYBER UDAYIPP*' , MessageType.text);
         }
         else {
             await message.client.sendMessage(message.jid,Config.PLK + '\n\n---------------------', MessageType.text);
@@ -43,7 +43,7 @@ else if (Config.WORKTYPE == 'public') {
         }
 
         if (Config.PLK == 'default') {
-            await message.client.sendMessage(message.jid,'Amalser Bot created by *Amal*' , MessageType.text);
+            await message.client.sendMessage(message.jid,'SARASU Bot created by *CYBER UDAYIPP*' , MessageType.text);
         }
         else {
             await message.client.sendMessage(message.jid,Config.PLK + '\n\n--------------------', MessageType.text);
